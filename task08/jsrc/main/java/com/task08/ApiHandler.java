529d622c97327b38c6cb3fd140e46761061def3f
EPAM\Wahid_Shaikh <wahid_shaikh@epam.com>
2024-09-18 12:16:49 +0530

    final changes

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -3,25 +3,38 @@ package com.task08;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+import com.weather.WeatherData;
+import org.json.simple.JSONObject;
 
 @LambdaHandler(
     lambdaName = "api_handler",
 	roleName = "api_handler-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}",
+	layers = {"sdk-layer"},
+	runtime = DeploymentRuntime.JAVA17,
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
+@LambdaLayer(
+		layerName = "sdk-layer",
+		libraries = {"lib/WeatherAPI.jar"},
+		runtime = DeploymentRuntime.JAVA17
+)
+@LambdaUrlConfig(
+		authType = AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED
+)
+
+public class ApiHandler implements RequestHandler<Object, JSONObject> {
+
+	public JSONObject handleRequest(Object request, Context context) {
 		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
+		WeatherData weatherData = new WeatherData();
+        return weatherData.getWeatherData();
 	}
 }
