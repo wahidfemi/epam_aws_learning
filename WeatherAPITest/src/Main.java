40b72e7039865293c01a24552467ad6f64b19927
EPAM\Wahid_Shaikh <wahid_shaikh@epam.com>
2024-09-18 18:11:14 +0530

    weather api test java application

---
+++ WeatherAPITest/src/Main.java
@@ -0,0 +1,10 @@
+import com.weather.WeatherData;
+
+public class Main {
+    public static void main(String[] args) {
+
+        WeatherData weatherData = new WeatherData();
+        System.out.println(weatherData.getWeatherData());
+
+    }
+}
