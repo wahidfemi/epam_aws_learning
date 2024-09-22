5e989041f69994655da28dc1c6131b7bd2eab3f9
EPAM\Wahid_Shaikh <wahid_shaikh@epam.com>
2024-09-22 11:36:49 +0530

    added check for overlap reservation

--- task10/jsrc/main/java/com/task10/handler/PostReservationsHandler.java
+++ task10/jsrc/main/java/com/task10/handler/PostReservationsHandler.java
@@ -15,8 +15,7 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import org.json.JSONObject;
 
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 public class PostReservationsHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
@@ -34,19 +33,40 @@ public class PostReservationsHandler implements RequestHandler<APIGatewayProxyRe
         //reading tables table data to check whether reservation table # exists or not
         String tablesTableName = System.getenv("tables_table");
         AmazonDynamoDBAsync dynamoDBClient = AmazonDynamoDBAsyncClientBuilder.standard().withRegion(region).build();
-        ScanRequest scanRequest = new ScanRequest()
-                .withTableName(tablesTableName);
-        ScanResult result = dynamoDBClient.scan(scanRequest);
+        ScanRequest tablesScanRequest = new ScanRequest().withTableName(tablesTableName);
+        ScanResult tablesResult = dynamoDBClient.scan(tablesScanRequest);
         boolean tableExists = false;
         System.out.println("reservation table number : "+reservationsData.get("tableNumber").toString());
-        for (Map<String, AttributeValue> item : result.getItems()) {
+        for (Map<String, AttributeValue> item : tablesResult.getItems()) {
             System.out.println("existing table id : "+item.get("number").getN());
             if(item.get("number").getN().equalsIgnoreCase(reservationsData.get("tableNumber").toString())){
                 tableExists = true;
             }
         }
 
-        if(tableExists) {
+        //reading reservations table data to check for any reservation overlap
+        boolean isOverlaps = false;
+        ScanRequest reservationsScanRequest = new ScanRequest().withTableName(reservationsTableName);
+        ScanResult reservationsResult = dynamoDBClient.scan(reservationsScanRequest);
+        List<Map<String, AttributeValue>> existingReservations = new ArrayList<Map<String, AttributeValue>>();
+        for (Map<String, AttributeValue> item : reservationsResult.getItems()) {
+            if(item.get("tableNumber").getN().equalsIgnoreCase(reservationsData.get("tableNumber").toString())
+            && item.get("date").getN().equalsIgnoreCase(reservationsData.get("date").toString())){
+                existingReservations.add(item);
+            }
+        }
+
+        for(Map<String, AttributeValue> existingReservation: existingReservations){
+            int slot_time_start = Integer.parseInt(reservationsData.get("slotTimeStart").toString().substring(0,2));
+            int slot_time_end = Integer.parseInt(reservationsData.get("slotTimeEnd").toString().substring(0,2));
+            int existing_time_start = Integer.parseInt(existingReservation.get("slotTimeStart").getS().substring(0,2));
+            int existing_time_end = Integer.parseInt(existingReservation.get("slotTimeEnd").getS().substring(0,2));
+            if (slot_time_start < existing_time_end && slot_time_end > existing_time_start){
+                isOverlaps = true;
+            }
+        }
+
+        if(tableExists && !isOverlaps) {
             DynamoDB dynamoDB = new DynamoDB(dynamoDBClient);
             Table table = dynamoDB.getTable(reservationsTableName);
 
