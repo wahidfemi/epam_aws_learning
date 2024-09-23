eae7ff8b8b7a3b87868eb950c64a6d9f95d8c9f6
EPAM\Wahid_Shaikh <wahid_shaikh@epam.com>
2024-09-24 00:47:59 +0530

    initial commit for task 11 - repeating from scratch

---
+++ task11/jsrc/main/java/com/task10/handler/GetReservationsHandler.java
@@ -0,0 +1,58 @@
+
+package com.task10.handler;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.json.JSONObject;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class GetReservationsHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
+
+        System.out.println("going to get reservations data from dynamodb table");
+        String reservationsTableName = System.getenv("reservations_table");
+        String region = System.getenv("REGION");
+        AmazonDynamoDBAsync dynamoDBClient = AmazonDynamoDBAsyncClientBuilder.standard().withRegion(region).build();
+
+        ScanRequest scanRequest = new ScanRequest()
+                .withTableName(reservationsTableName);
+                //.withAttributesToGet("tableNumber", "clientName", "phoneNumber", "date", "slotTimeStart", "slotTimeEnd");
+        ScanResult result = dynamoDBClient.scan(scanRequest);
+
+        ArrayList<Object> tempList = new ArrayList<Object>();
+        for (Map<String, AttributeValue> item : result.getItems()) {
+            Map<String, Object> simpleMap = new HashMap<String, Object>();
+            simpleMap.put("tableNumber", Integer.parseInt(item.get("tableNumber").getN()));
+            simpleMap.put("clientName", item.get("clientName").getS());
+            simpleMap.put("phoneNumber", item.get("phoneNumber").getS());
+            simpleMap.put("date", item.get("date").getS());
+            simpleMap.put("slotTimeStart", item.get("slotTimeStart").getS());
+            simpleMap.put("slotTimeEnd", item.get("slotTimeEnd").getS());
+            tempList.add(new JSONObject(simpleMap));
+        }
+
+        return new APIGatewayProxyResponseEvent()
+                .withStatusCode(200)
+                .withBody(new JSONObject().put("reservations", tempList).toString());
+    }
+
+}
---
+++ task11/jsrc/main/java/com/task10/handler/GetTablesHandler.java
@@ -0,0 +1,51 @@
+
+package com.task10.handler;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetTablesHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
+
+        System.out.println("going to get tables data from dynamodb table");
+        String tablesTableName = System.getenv("tables_table");
+        String region = System.getenv("REGION");
+        AmazonDynamoDBAsync dynamoDBClient = AmazonDynamoDBAsyncClientBuilder.standard().withRegion(region).build();
+
+        ScanRequest scanRequest = new ScanRequest()
+                .withTableName(tablesTableName);
+                //.withAttributesToGet("id", "number", "places", "isVip", "minOrder");
+
+        ScanResult result = dynamoDBClient.scan(scanRequest);
+
+        ArrayList<Object> tempList = new ArrayList<Object>();
+        for (Map<String, AttributeValue> item : result.getItems()) {
+            Map<String, Object> simpleMap = new HashMap<String, Object>();
+            simpleMap.put("id", Integer.valueOf(item.get("id").getN()));
+            simpleMap.put("number", Integer.valueOf(item.get("number").getN()));
+            simpleMap.put("places", Integer.valueOf(item.get("places").getN()));
+            simpleMap.put("isVip", item.get("isVip").getBOOL());
+            simpleMap.put("minOrder", Integer.valueOf(item.get("minOrder").getN()));
+            tempList.add(new JSONObject(simpleMap));
+        }
+
+        return new APIGatewayProxyResponseEvent()
+                .withStatusCode(200)
+                .withBody(new JSONObject().put("tables", tempList).toString());
+    }
+
+}
